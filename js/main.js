33f16659608d86880ae100d90234d8d35e6b404e
RabbyHasanSaref <rabbyhasansaref83@gmail.com>
2024-02-16 19:40:00 +0600

    js app

--- js/main.js
+++ js/main.js
@@ -1,1 +1,35 @@
-console.log('Hello World')
+// Triangle 
+function TriangleBase(){
+    // Triangle-Base
+    const triangleBase = document.getElementById('triangle-base');
+    const triangleBaseData = triangleBase.value;
+    const triangleBaseDataConverter = parseFloat(triangleBaseData);
+  
+    // Triangle-Heigth 
+    const triangleHeight = document.getElementById('triangle-height');
+    const triangleHeigthData = triangleHeight.value;
+    const triangleHeigthDataConverter = parseFloat(triangleHeigthData);
+
+    // Triangle Finel Result 
+    const area = triangleBaseDataConverter * triangleHeigthDataConverter / 2;
+    const triangleResult = document.getElementById('triangle-result');
+    triangleResult.innerText = area;
+}
+
+// Ractangle
+ document.getElementById('rectangle-calculate').addEventListener('click', function(){
+    // Rectangle Width
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const rectangleWidthData = rectangleWidth.value;
+    const rectangleWidthDataConverter = parseFloat(rectangleWidthData);
+    
+    // Rectangle Length
+    const rectangleLength = document.getElementById('rectangle-Length');
+    const rectangleLengthData = rectangleLength.value;
+    const rectangleLengthDataConverter = parseFloat(rectangleLengthData);
+    
+    // Rectangle Result
+    const area = rectangleWidthDataConverter * rectangleLengthDataConverter;
+    const rectangleResult = document.getElementById('rectangle-result');
+    rectangleResult.innerText = area;
+ })
